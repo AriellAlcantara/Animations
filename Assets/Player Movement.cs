35c1207c7d70a01712d5a8c0cad42fada2fba3bd
AriellAlcantara <aidenpearce2121@gmail.com>
2023-07-12 15:03:27 +0800

    Animation part 3

--- Assets/Player Movement.cs
+++ Assets/Player Movement.cs
@@ -11,6 +11,8 @@ public class PlayerMovement : MonoBehaviour
     public Vector2 movementInput;
     public Rigidbody2D rigidBody;
     public Animator anim;
+    public int coinCounter;
+    public int healthPoints;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
@@ -20,44 +22,59 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        //if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        //{
+        //    anim.enabled = true;
+        //    anim.SetTrigger("ForwardAnimation");
+        //}
+        //if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
+        //{
+        //    anim.enabled = false;
+        //}
+        //if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        //{
+        //    anim.enabled = true;
+        //    anim.SetTrigger("BackwardAnimation");
+        //}
+        //if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
+        //{
+        //    anim.enabled = false;
+        //}
+        //if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        //{
+        //    anim.enabled = true;
+        //    anim.SetTrigger("LeftAnimation");
+        //}
+        //if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+        //{
+        //    anim.enabled = false;
+        //}
+        //if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        //{
+        //    anim.enabled = true;
+        //    anim.SetTrigger("RightAnimation");
+        //}
+        //if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+        //{
+        //    anim.enabled = false;
+        //}
+
+        anim.SetFloat("Horizontal", movementInput.x);
+        anim.SetFloat("Vertical", movementInput.y);
+        anim.SetFloat("Speed", movementInput.sqrMagnitude);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Coins"))
         {
-            anim.enabled = true;
-            anim.SetTrigger("ForwardAnimation");
-        }
-        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            anim.enabled = false;
-        }
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            anim.enabled = true;
-            anim.SetTrigger("BackwardAnimation");
-        }
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
-        {
-            anim.enabled = false;
-        }
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            anim.enabled = true;
-            anim.SetTrigger("LeftAnimation");
-        }
-        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
-        {
-            anim.enabled = false;
-        }
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            anim.enabled = true;
-            anim.SetTrigger("RightAnimation");
-        }
-        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            anim.enabled = false;
+            coinCounter++;
+            Destroy(collision.gameObject);
         }
     }
-    
+
+
+
     private void FixedUpdate()
 
     {
